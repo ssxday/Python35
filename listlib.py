847a438e94447d5ddb9dd5cf6a364a6cab21ff6a
ssxday <AUG@AuggiedeMacBook-Pro.local>
2017-01-02 22:37:49 +0800

    nothing

--- listlib.py
+++ listlib.py
@@ -3,48 +3,32 @@ lib = ['abp', 'abs', 'aby', 'adn', 'adz', 'aikb', 'akaiene',
        'bcv', 'beb', 'bid', 'bijn', 'bist', 'blk', 'bobb',
        'bta', 'bur', 'cead', 'cesd', 'cetd', 'chs', 'cjob',
        'cjod', 'crc', 'crpd', 'crs', 'dandy', 'dasd', 'ddt',
-       'dpmi', 'drc', 'drg', 'dsam', 'dsot', 'dvaj', 'dvdes',
-       'dwd', 'ebod', 'ekdv', 'eyan', 'ezd', 'fset', 'gar',
-       'gets', 'gvg', 'hbad', 'heyzo', 'hiz', 'hmgl', 'hnd',
-       'hodv', 'hzgd', 'iene', 'iesp', 'iptd', 'ipz', 'jbs',
-       'juc', 'jufd', 'jux', 'kawd', 'kird', 'kisd', 'kmi',
-       'lxvs', 'mdb', 'mds', 'mdyd', 'mek', 'meyd', 'miad',
-       'midd', 'mide', 'migd', 'mild', 'mimk', 'mird', 'momj',
-       'mot', 'mvsd', 'mxgs', 'mxsps', 'natr', 'nfdm', 'nitr',
-       'nld', 'nnpj', 'npv', 'nrs', 'nwf', 'oba', 'odfm',
-       'odfr', 'okad', 'oksn', 'once', 'oned', 'onez', 'onsd',
-       'osr', 'pgd', 'pjd', 'pppd', 'psd', 'rbd', 'rct', 'rdd',
-       'rki', 'saba', 'sace', 'sama', 'sbmx', 'scop', 'sdde',
+       'dic', 'diy', 'dpmi', 'drc', 'drg', 'dsam', 'dsot',
+       'dvaj', 'dvdes', 'dwd', 'ebod', 'ecb', 'ekdv', 'ekw',
+       'eyan', 'ezd', 'fset', 'gar', 'gets', 'gvg', 'hbad',
+       'heyzo', 'hiz', 'hmgl', 'hnd', 'hodv', 'hzgd', 'iene',
+       'iesp', 'iptd', 'ipz', 'jbs', 'juc', 'jufd', 'jux',
+       'juy', 'kawd', 'kird', 'kisd', 'kmi', 'lxvs', 'mdb',
+       'mds', 'mdyd', 'mek', 'meyd', 'miad', 'midd', 'mide',
+       'migd', 'mild', 'mimk', 'mird', 'momj', 'mot', 'mvsd',
+       'mxgs', 'mxsps', 'natr', 'nfdm', 'nitr', 'nld', 'nnpj',
+       'npv', 'nrs', 'nwf', 'oba', 'odfm', 'odfr', 'okad',
+       'oksn', 'once', 'oned', 'onez', 'onsd', 'osr', 'pgd',
+       'pjd', 'pppd', 'psd', 'rbd', 'rct', 'rdd', 'rki',
+       'saba', 'sace', 'sama', 'sbmx', 'scop', 'sdde',
        'sddm', 'sdmt', 'sdmu', 'sdsi', 'sero', 'sga', 'sgv',
        'shkd', 'sma', 'snis', 'sod', 'soe', 'sora', 'spb',
        'sprd', 'srs', 'sspd', 'ssr', 'star', 'supd', 'svdvd',
        'tbl', 'tek', 'tgav', 'tmvi', 'tppn', 'tyod', 'ufd',
        'umd', 'upsm', 'ure', 'vagu', 'vdd', 'vec', 'vema',
        'venu', 'vgd', 'voss', 'vrtm', 'wanz', 'wat', 'whx',
-       'wnz', 'wpc', 'wss', 'xrw', 'ymn', 'yrh', 'yrz', 'ysn',
-       'yst', 'zex', 'zuko']
+       'wnz', 'wpc', 'wss', 'xrw', 'xvsr', 'ymn', 'yrh',
+       'yrz', 'ysn', 'yst', 'zex', 'zuko']
+
 
 lib = sorted(list(set(lib)))
 # print(lib)
 # print(len(lib))
 lib += ['aka', 'chn', 'cwp', 'dv', 'sw', 'wan', 'digi', 'send', 'bf', 'job',
         'hunt', 'pla', 'real']
-# a = 'haha'
-# for i in range(20):
-#     print('%*s' % (i,a))
-
-
-# a = 'hahaha'
-# for i in range(10):
-#     print('%*s'%(i+len(a),a))
-
-# def func(n):
-#     return n
-#
-# def test(func,n):
-#
-#     return func(n)+4
-#
-# print(test(func,10))
-
 
