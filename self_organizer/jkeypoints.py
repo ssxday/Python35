1b40df6500640b047df89352a7473e987c249e72
AUG <admin@mypycharm.com>
2017-02-24 17:51:01 +0800

    use session but failed

--- self_organizer/jkeypoints.py
+++ self_organizer/jkeypoints.py
@@ -19,9 +19,10 @@ Description:提供一个识别码，返回其相关联的信息
 import requests
 import re
 import json
+import math
 from common_use import Jp
 from common_use import Headers
-from random import choice
+from random import choice, random
 from time import sleep
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
@@ -34,7 +35,7 @@ class SymbolSwitch:
     ^n\d{5}$                 TH格式
     """
     re_letters = re.compile(r'^\d?[a-z]{1,5}', re.I)
-    re_nums = re.compile(r'\d{3,6}-?\d{0,3}$')
+    re_nums = re.compile(r'(\d{3,6})-?(\d{0,3})')
 
     def __init__(self, txt=''):
         letters = self.re_letters.search(txt)
@@ -42,16 +43,15 @@ class SymbolSwitch:
         self.symbol = txt  # 最终要的是它
         if letters and nums:
             letters = letters.group()
-            nums = nums.group()
             # 下面详细处理letters和nums
             if letters.lower() == 'n':
-                self.symbol = '{}{}'.format(letters, nums)
+                self.symbol = '{}{}'.format(letters, nums.group())
             elif letters.lower() == 'carib':
-                self.symbol = '{}-{}'.format(nums[:6], nums[6:])
+                self.symbol = '{}-{}'.format(nums.group(1), nums.group(2))
             elif letters.lower() == '1pond':
-                self.symbol = '{}_{}'.format(nums[:6], nums[6:])
+                self.symbol = '{}_{}'.format(nums.group(1), nums.group(2))
             else:
-                self.symbol = '{}-{}'.format(letters.upper(), nums)
+                self.symbol = '{}-{}'.format(letters.upper(), nums.group())
         else:
             raise WeaknessError('输入的内容无法转化为标准格式')
 
@@ -74,10 +74,10 @@ class Scan:
         self.__scan()
 
     def __scan(self):
-        self.info.setdefault(3, self.__get_title())
-        self.info.setdefault(5, self.__get_image())
-        self.info.setdefault(7, self.__get_performer())
-        self.info.setdefault(2, self.__get_publish_date())
+        self.info.setdefault(5, self.__get_title())
+        self.info.setdefault(7, self.__get_image())
+        self.info.setdefault(11, self.__get_performer())
+        self.info.setdefault(3, self.__get_publish_date())
 
     def __get_title(self):
         title_tag = self.__soup.find('h3')
@@ -110,28 +110,68 @@ class Scan:
 
 class Describer:
     def __init__(self, symbol, to_json=False):
-        host = choice(Jp.HOSTS)  # 随机选择一个base_url
-        req = SymbolSwitch(symbol).symbol  # 请求字符串
-        infos = {1: req}
-        page = requests.get(urljoin(host, req), headers=Headers.HEADERS)  # 引入头信息设置
-        if page:
-            page.encoding = 'utf-8'
-            sourcecode = page.text
-            infos.update(Scan(sourcecode).info)
-        if to_json:
-            self.info = json.dumps(infos)
+        self.__host = choice(Jp.HOSTS)  # 随机选择一个base_url
+        self.__req = SymbolSwitch(symbol).symbol  # 请求字符串
+        self.__to_json = to_json
+        self.__sess = requests.Session()  # 使用session
+        self.__htm = self.__get_htm()  # 页面源代码
+        # self.__get_mag_links()  # 获取并解析ajax内容
+
+    def __get_htm(self):
+        page_htm = self.__sess.get(urljoin(self.__host, self.__req), headers=Headers.HEADERS, timeout=20)  # 引入头信息设置
+        if page_htm:
+            page_htm.encoding = 'utf-8'
+            sourcecode = page_htm.text
+            print(page_htm.cookies)
+            return sourcecode
         else:
-            self.info = infos
+            raise WeaknessError('Failed to open the page.')
+
+    def __get_mag_links(self):
+        """首先要取到ajax要发送的5个参数"""
+        gid = re.search(r'var gid = (\d*?);', self.__htm).group(1)
+        lang = 'zh'
+        uc = re.search(r'var uc = (\d*?);', self.__htm).group(1)
+        img = re.search(r"var img = '(.*?)';", self.__htm).group(1)
+        floor = math.floor(random() * 1e3 + 1)
+
+        ajax_url = self.__host + '/ajax/uncledatoolsbyajax.php?gid=%s&lang=%s&img=%s&uc=%s&floor=%s' % (
+            gid, lang, img, uc, floor)
+        print(ajax_url)
+        # ajax的response
+        ajax_result = self.__sess.get(ajax_url, headers=Headers.HEADERS)
+        if ajax_result:
+            ajax_result.encoding = 'utf-8'
+            print(ajax_result.text)
+            soup = BeautifulSoup(ajax_result.text, 'lxml')
+            trs = soup.find_all('tr')  # 链接以tr标签为单元
+            if trs:  # 返回了实质内容
+                self.__magnet = [tr.td.a.get('href', 'Unknow') for tr in trs]  # 推导
+            else:  # 没返回实质内容
+                raise WeaknessError('AJAX Failed to fetch the magnet links')
+
+    @property
+    def infos(self):
+        """"""
+        details = {1: self.__req, }
+        details.update(Scan(self.__htm).info)
+        if self.__to_json:
+            return json.dumps(details)
+        return details
+
+    def __del__(self):
+        self.__sess.close()
 
 
 relationship = {
-    1: 'symbol', 3: 'title', 5: 'image',
-    7: 'performers', 2: 'publish',
+    1: 'symbol', 5: 'title', 7: 'image',
+    11: 'performers', 3: 'publish',
+    13: 'magnet',
 }
 
 
 def describe(what, to_json=False):
-    return Describer(what, to_json).info
+    return Describer(what, to_json).infos
 
 
 def desc_walk(begin, end, head='abp', to_json=False):
